0dae64c09a05bb77e507c6c640cf0c804bf911da
JuanFranciscoEstrada14 <juan14estrada@gmail.com>
2024-03-07 17:09:13 -0300

    ain

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 class Product {
     constructor(id, title, description, price, img, code, stock) {
         this.id = id;
@@ -11,49 +13,64 @@ class Product {
 }
 
 class ProductManager {
-    static ultId = 0;
+    constructor(path) {
+        this.path = path;
+        this.products = [];
+        this.loadProducts();
+    }
 
-    constructor () {
-        this.products = []
+    loadProducts() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf8');
+            this.products = JSON.parse(data);
+        } catch (err) {
+            console.error('Error al cargar los productos:', err);
+        }
+    }
+
+    saveProducts() {
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2));
+        } catch (err) {
+            console.error('No se guardaron los productos:', err);
+        }
     }
 
     addProduct(title, description, price, img, code, stock) {
-        if(!title || !description || !price || !img || !code || !stock  ) {
-            console.log ("Completar todos los datos es obligatorio");
+        if (!title || !description || !price || !img || !code || !stock) {
+            console.error("Completar todos los datos es obligatorio");
             return;
         }
 
         if (this.products.some(product => product.code === code)) {
-            console.error('Este código ya está en uso.');
+            console.error('El código ya está en uso.');
             return;
         }
 
-        const product = new Product(++ProductManager.ultId, title, description, price, img, code, stock);
+        const product = new Product(this.products.length + 1, title, description, price, img, code, stock);
         this.products.push(product);
+        this.saveProducts();
     }
 
     getProducts() {
         return this.products;
     }
 
-    getProductById (id) {
+    getProductById(id) {
         const product = this.products.find(item => item.id === id);
-        
-        if(!product) {
-            console.log("No se encontró este producto")
+        if (!product) {
+            console.log("No se encontró este producto");
         } else {
             console.log("Se encontró!", product);
         }
     }
 }
 
-const manager = new ProductManager () ;
+
+const manager = new ProductManager('productos.json');
 
 console.log(manager.getProducts());
 
-manager.addProduct("Producto prueba" , "Este es un producto prueuba", 200, "sin imagen", "abc123" , 25);
+manager.addProduct("Producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc123", 25);
 
 console.log(manager.getProducts());
-
-manager.getProductById(1);
-manager.getProductById(33);
